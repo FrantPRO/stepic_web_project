ece6073538db4c8693ef0ef889d44b8350e2045f
Французов Станислав <user1323@realweb.ru>
2020-07-19 12:54:17 +0300

    step 1.9 fix 5

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
-sudo gunicorn -c /etc/gunicorn.d/hello.py hello:application
+sudo gunicorn -c /etc/gunicorn.d/hello.py hello:app
 sudo /etc/init.d/gunicorn restart
