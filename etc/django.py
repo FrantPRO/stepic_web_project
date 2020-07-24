8cb2baefa560644652f0f3546cd7ef9275160539
Французов Станислав <user1323@realweb.ru>
2020-07-24 21:01:25 +0300

    step 2.1 fix 1

---
+++ etc/django.py
@@ -0,0 +1,4 @@
+pythonpath = '/home/box/web/ask'
+python = '/usr/bin/python3'
+bind = "0.0.0.0:8000"
+workers = 4
--- init.sh
+++ init.sh
@@ -2,5 +2,7 @@ sudo rm /etc/nginx/sites-available/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-available/default
 sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -sf /home/box/web/etc/django.py /etc/gunicorn.d/django.py
 sudo gunicorn -c /etc/gunicorn.d/hello.py hello:app
+sudo gunicorn -c /etc/gunicorn.d/django.py ask:app
 sudo /etc/init.d/gunicorn restart
